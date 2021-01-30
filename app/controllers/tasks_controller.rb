dde84e34f29b3b861563778f20bd0e6b37e65eca
Alex Schwartz <aschwartz@detaso.com>
2021-01-30 13:18:53 -0600

    Completed intermission work tutorial

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+
+    task.save
+
+    redirect_to '/tasks'
+  end
+
+  def show
+    @task = find_by(params[:id])
+  end
+
+  def edit
+    @task = find_by(params[:id])
+  end
+
+  def update
+    task = find_by(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+
+    task.save
+
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+
+    redirect_to '/tasks'
+  end
+
+  private
+
+  def find_by(id)
+    Task.find(id)
+  end
+end
